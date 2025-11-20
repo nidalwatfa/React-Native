df9bed65b62563a77cc9c9f0c1cb2e4c72c3c8c1
nidal Watfa <166144521+nidalwatfa@users.noreply.github.com>
2025-11-20 22:08:16 +0300

    Update babelel.config.js

--- babelel.config.js
+++ babelel.config.js
@@ -1,6 +1,7 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // لازم نضيف هذا الـ plugin عشان React Navigation يشتغل
+    'react-native-reanimated/plugin',
+  ],
 };
