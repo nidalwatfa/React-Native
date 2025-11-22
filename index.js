0baec3d9214f6e24e2cdf81918c0d9c7554342bb
nidal Watfa <166144521+nidalwatfa@users.noreply.github.com>
2025-11-22 16:02:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-import {AppRegistry} from 'react-native';
+
+import { I18nManager } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+// تفعيل RTL للعربية
+I18nManager.allowRTL(true);
+I18nManager.forceRTL(true);
 
 AppRegistry.registerComponent(appName, () => App);
