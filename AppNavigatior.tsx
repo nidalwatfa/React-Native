bd82ed1e1fba64b2c2f16072e01c6d919b411373
nidal Watfa <166144521+nidalwatfa@users.noreply.github.com>
2025-11-25 23:55:52 +0300

    Update AppNavigatior.tsx

--- AppNavigatior.tsx
+++ AppNavigatior.tsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "../screens/HomeScreen";
@@ -7,12 +6,16 @@ import SettingsScreen from "../screens/SettingsScreen";
 import AboutScreen from "../screens/AboutScreen";
 import ContactScreen from "../screens/ContactScreen";
 import DashboardScreen from "../screens/DashboardScreen";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
 
 const Drawer = createDrawerNavigator();
 
 export default function AppNavigator() {
   return (
     <Drawer.Navigator>
+      <Drawer.Screen name="Login" component={LoginScreen} />
+      <Drawer.Screen name="Register" component={RegisterScreen} />
       <Drawer.Screen name="Dashboard" component={DashboardScreen} />
       <Drawer.Screen name="Home" component={HomeScreen} />
       <Drawer.Screen name="Profile" component={ProfileScreen} />
