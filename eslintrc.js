cc655fa7b5a6b71d50c6b4671b081d3d7ae1cf5d
nidal Watfa <166144521+nidalwatfa@users.noreply.github.com>
2025-11-25 21:22:02 +0300

    Create eslintrc.js

---
+++ eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    "@react-native-community",
+    "plugin:react/recommended",
+    "plugin:react-native/all"
+  ],
+  rules: {
+    "react-native/no-inline-styles": "off",
+    "react/react-in-jsx-scope": "off"
+  }
+};
