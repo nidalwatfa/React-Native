20f865556c8df85378c54c042309a02b2d312128
nidal Watfa <166144521+nidalwatfa@users.noreply.github.com>
2025-11-25 23:18:15 +0300

    Update HomeScreen.tsx

--- HomeScreen.tsx
+++ HomeScreen.tsx
@@ -1,33 +1,22 @@
 
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import { globalStyles, colors } from "../styles";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <Icon name="home" size={50} color="#4CAF50" />
-      <Text style={styles.text}>مرحبًا بك في الشاشة الرئيسية 🎉</Text>
+    <View style={globalStyles.container}>
+      <Icon name="home" size={50} color={colors.primary} />
+      <Text style={globalStyles.title}>مرحبًا بك في الشاشة الرئيسية 🎉</Text>
       <Button
         title="اذهب إلى الملف الشخصي"
+        color={colors.secondary}
         onPress={() => navigation.navigate("Profile")}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginVertical: 20
-  }
-});
