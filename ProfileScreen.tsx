90c6d37934cc8a0195248233940614758d256eeb
nidal Watfa <166144521+nidalwatfa@users.noreply.github.com>
2025-11-25 23:20:58 +0300

    Update ProfileScreen.tsx

--- ProfileScreen.tsx
+++ ProfileScreen.tsx
@@ -1,26 +1,14 @@
-
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import { globalStyles, colors } from "../styles";
 
 export default function ProfileScreen() {
   return (
-    <View style={styles.container}>
-      <Icon name="person" size={50} color="#2196F3" />
-      <Text style={styles.text}>هذه شاشة الملف الشخصي 👤</Text>
+    <View style={globalStyles.container}>
+      <Icon name="person" size={50} color={colors.secondary} />
+      <Text style={globalStyles.title}>هذه شاشة الملف الشخصي 👤</Text>
+      <Text style={globalStyles.subtitle}>يمكنك تعديل بياناتك هنا</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginTop: 20
-  }
-});
