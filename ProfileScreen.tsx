119f6eb04fe505f1d1cdd4827521a00b1ff8bdeb
nidal Watfa <166144521+nidalwatfa@users.noreply.github.com>
2025-11-25 22:24:29 +0300

    Create ProfileScreen.tsx

---
+++ ProfileScreen.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function ProfileScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>هذه شاشة الملف الشخصي 👤</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
