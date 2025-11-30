fc97712214307149d8b3dc0b64e159d7024a8e48
nidal Watfa <166144521+nidalwatfa@users.noreply.github.com>
2025-11-30 18:26:23 +0300

    Create embed.js

---
+++ embed.js
@@ -0,0 +1,23 @@
+<!-- Embed saved Snack -->
+<div
+  data-snack-id="@react-navigation/basic-scrollview-tab-v3"
+  data-snack-platform="web"
+  data-snack-preview="true"
+  data-snack-theme="light"
+  data-snack-loading="lazy"
+  style="overflow:hidden;background:#fafafa;border:1px solid rgba(0,0,0,.08);border-radius:4px;height:505px;width:100%">
+</div>
+
+<!-- Embed inlined Snack -->
+<div
+  data-snack-code="console.log('hello%20world')%3B"
+  data-snack-dependencies="expo-constants%2Clodash%404"
+  data-snack-name="My%20Snack"
+  data-snack-description="My%20Amazing%20Snack"
+  data-snack-preview="true"
+  data-snack-platform="ios"
+  style="overflow:hidden;background:#fafafa;border:1px solid rgba(0,0,0,.08);border-radius:4px;height:505px;width:100%">
+</div>
+
+<!-- Load the embed.js script -->
+<script async src="https://snack.expo.dev/embed.js"></script>
