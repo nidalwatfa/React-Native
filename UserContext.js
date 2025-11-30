0fc3e94f40e8915ce8159ac8aa8469993fd8d345
nidal Watfa <166144521+nidalwatfa@users.noreply.github.com>
2025-11-30 17:13:42 +0300

    Create UserContext.js

---
+++ UserContext.js
@@ -0,0 +1,32 @@
+useState, useContext } from 'react';
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = (userData) => {
+    setUser(userData);
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, setUser, isLoggedIn, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (!context) {
+    throw new Error('useUser must be used within UserProvider');
+  }
+  return context;
+};
